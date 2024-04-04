e819fb0d9502b89e12c18a479e1e492b960876ce
Vharaun <33102576+vharaun@users.noreply.github.com>
2024-04-03 21:15:09 -0600

    Simply and Code Cleanup

--- Scripts/Custom/Modules/RisingTide/Object/MaritimeTradeCrate.cs
+++ Scripts/Custom/Modules/RisingTide/Object/MaritimeTradeCrate.cs
@@ -8,21 +8,17 @@ namespace Server.Items
 	[Flipable(0xA2C4, 0xA2C5)]
 	public class MaritimeTradeCrate : LockableContainer
 	{
+		public override int DefaultGumpID => 0x9CDF;
+		public override double DefaultWeight => 5.0;
+
 		[Constructable]
 		public MaritimeTradeCrate() : base(0xA2C4)
 		{
-			GumpID = 0x9CDF;
 		}
 
 		public MaritimeTradeCrate(Serial serial) : base(serial)
 		{
 		}
-
-		//public static void Initialize()
-		//{
-		//	TileData.ItemTable[0xA2C4].Flags |= TileFlag.Container;
-		//	TileData.ItemTable[0xA2C5].Flags |= TileFlag.Container;
-		//}
 
 		public override int DefaultMaxItems => 625;
 		public override int DefaultMaxWeight => 1600;
@@ -41,19 +37,7 @@ namespace Server.Items
 			var version = reader.ReadInt();
 		}
 	}
-}
 
-
-//using System;
-
-//using Server;
-//using Server.Items;
-//using Server.Mobiles;
-//using Server.Engines.CityLoyalty;
-//using Server.Engines.RisingTide;
-
-namespace Server.Items
-{
 	public enum CargoQuality
 	{
 		Grandmaster,
@@ -79,6 +63,8 @@ namespace Server.Items
 	[Flipable(0xA2C4, 0xA2C5)]
 	public class MaritimeCargo : Item
 	{
+		public override double DefaultWeight => 5.0;
+
 		private CargoQuality _CargoQuality;
 		private CargoType _CargoType;
 		private Town _City;
@@ -86,7 +72,7 @@ namespace Server.Items
 		[CommandProperty(AccessLevel.GameMaster)]
 		public CargoQuality CargoQuality
 		{
-			get { return _CargoQuality; }
+			get => _CargoQuality;
 			set
 			{
 				_CargoQuality = value;
@@ -103,7 +89,7 @@ namespace Server.Items
 		[CommandProperty(AccessLevel.GameMaster)]
 		public CargoType CargoType
 		{
-			get { return _CargoType; }
+			get => _CargoType;
 			set
 			{
 				_CargoType = value;
@@ -118,7 +104,7 @@ namespace Server.Items
 		}
 
 		[CommandProperty(AccessLevel.GameMaster)]
-		public Town City { get { return _City; } set { _City = value; InvalidateProperties(); } }
+		public Town City { get => _City; set { _City = value; InvalidateProperties(); } }
 
 		[Constructable]
 		public MaritimeCargo()
@@ -166,40 +152,23 @@ namespace Server.Items
 		private static CargoType RandomType()
 		{
 			return Utility.RandomEnum<CargoType>();
-			//switch (Utility.Random(8))
-			//{
-			//	default:
-			//	case 0: return CargoType.Cloth;
-			//	case 1: return CargoType.Jewelry;
-			//	case 2: return CargoType.Wood;
-			//	case 3: return CargoType.Metal;
-			//	case 4: return CargoType.Munitions;
-			//	case 5: return CargoType.Granite;
-			//	case 6: return CargoType.Reagents;
-			//	case 7: return CargoType.Glassware;
-			//}
 		}
 
 		public int GetAwardAmount()
 		{
-			int amount;
-
-			switch (_CargoQuality)
+			var amount = _CargoQuality switch
 			{
-				default:
-				case CargoQuality.Grandmaster: amount = Utility.RandomMinMax(100, 200); break;
-				case CargoQuality.Exalted: amount = Utility.RandomMinMax(500, 600); break;
-				case CargoQuality.Legendary: amount = Utility.RandomMinMax(1000, 1100); break;
-				case CargoQuality.Mythical: amount = Utility.RandomMinMax(10000, 15000); break;
-			}
-
+				CargoQuality.Grandmaster => Utility.RandomMinMax(100, 200),
+				CargoQuality.Exalted => Utility.RandomMinMax(500, 600),
+				CargoQuality.Legendary => Utility.RandomMinMax(1000, 1100),
+				CargoQuality.Mythical => Utility.RandomMinMax(10000, 15000),
+				_ => 0,
+			};
 			return amount;
 		}
 
 		public override void AddNameProperty(ObjectPropertyList list)
 		{
-			//list.Add(1158906, String.Format("#{0}", CityLoyaltySystem.GetCityLocalization(_City).ToString())); // Maritime Trade Cargo Destined for ~1_CITY~
-			//list.Add(1158906, $"{CityLoyaltySystem.GetCityLocalization(_City):#}"); // Maritime Trade Cargo Destined for ~1_CITY~
 			list.Add(1158906, $"{City:#}");
 		}
 
@@ -212,18 +181,18 @@ namespace Server.Items
 
 		public static int TypeLabel(CargoType type)
 		{
-			switch (type)
+			return type switch
 			{
-				default:
-				case CargoType.Cloth: return 1044286;
-				case CargoType.Jewelry: return 1011172;
-				case CargoType.Wood: return 1079435;
-				case CargoType.Metal: return 1049567;
-				case CargoType.Munitions: return 1158902;
-				case CargoType.Granite: return 1158900;
-				case CargoType.Reagents: return 1002127;
-				case CargoType.Glassware: return 1158901;
-			}
+				CargoType.Jewelry => 1011172,
+				CargoType.Wood => 1079435,
+				CargoType.Metal => 1049567,
+				CargoType.Munitions => 1158902,
+				CargoType.Granite => 1158900,
+				CargoType.Reagents => 1002127,
+				CargoType.Glassware => 1158901,
+				CargoType.Cloth => 1044286,
+				_ => 0,
+			};
 		}
 
 		public MaritimeCargo(Serial serial) : base(serial)
@@ -233,7 +202,7 @@ namespace Server.Items
 		public override void Serialize(GenericWriter writer)
 		{
 			base.Serialize(writer);
-			writer.Write(0);
+			writer.Write(0); //version placeholder
 
 			writer.Write((int)_CargoQuality);
 			writer.Write((int)_CargoType);
@@ -243,7 +212,7 @@ namespace Server.Items
 		public override void Deserialize(GenericReader reader)
 		{
 			base.Deserialize(reader);
-			int version = reader.ReadInt();
+			_ = reader.ReadInt(); //version placeholder
 
 			_CargoQuality = (CargoQuality)reader.ReadInt();
 			_CargoType = (CargoType)reader.ReadInt();
